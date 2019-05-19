a7581aa550dd92ca1baa352abaa35bde32a3aad8
David Stevenson <dstevenson@uplandsoftware.com>
2019-05-19 22:54:27 +0100

    Preparation for editing the published state of a song

--- lib/NeverTire/Table/Song/List.pm
+++ lib/NeverTire/Table/Song/List.pm
@@ -5,6 +5,8 @@ use namespace::autoclean;
 use NeverTire::Table::Moose;
 extends 'NeverTire::Table::Base';
 
+use DateTime;
+
 sub _build_resultset {
     my $self = shift;
 
@@ -29,9 +31,27 @@ has_column date_created => (
     sortable     => 1,
 );
 
+# TODO css to highlight whether it's in the future/past?
 has_column date_published => (
     header       => 'Published',
     sortable     => 1,
+);
+
+has_column action => (
+    content => sub {
+        my ($col, $table, $row) = @_;
+
+        # TODO Do these as links when the editor is implemented
+        my $pub = $row->date_published;
+        my $now = DateTime->now;
+        if ( defined($pub) && DateTime->compare($pub, $now) < 1 ) {
+            return 'Hide';
+        }
+        else {
+            return 'Show';
+        }
+    },
+
 );
 
 has '+empty_text' => (default => 'No songs are in the datbase');
