0171cf33b846b95747260d43750aab4e4ce8348c
David Stevenson <dstevenson@uplandsoftware.com>
2019-05-27 22:56:21 +0100

    Rename text controller to markdown

--- lib/NeverTire/Controller/Markdown.pm
+++ lib/NeverTire/Controller/Markdown.pm
@@ -1,4 +1,4 @@
-package NeverTire::Controller::Text;
+package NeverTire::Controller::Markdown;
 use Mojo::Base 'Mojolicious::Controller';
 
 # $r  = routes
@@ -9,10 +9,10 @@ use Text::Markdown qw/ markdown /;
 sub add_routes {
     my ($c, $r, $rl) = @_;
 
-    my $ul = $rl->any('/text')->to(controller => 'text');
+    my $ul = $rl->any('/markdown')->to(controller => 'markdown');
 
     # These are admin functions, need to be logged in for all of them.
-    $ul->route('/render')->name('render_markdown')->via('GET', 'POST')->to(action => 'render_markdown');
+    $ul->route('/render')->name('render_markdown')->via('POST')->to(action => 'render_markdown');
 }
 
 sub render_markdown {
--- public/never-tire.js
+++ public/never-tire.js
@@ -2,10 +2,10 @@ function markdownPreview(textAreaID, previewAreaID) {
     $(textAreaID).on('change keyup paste focus', $.throttle( 500, function() {
         $.ajax({
             type: "POST",
-            url: "/text/render",
+            url: "/markdown/render",
             data: { markdown: $(this).val() },
             success: function(data) {
-                $(previewAreaID).html(data);
+                $(previewAreaID).html(data).show();
             },
         });
     }));
