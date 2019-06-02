7d4e6dfc3a8fc5cfa5b43620d18c47d25ae8f64c
David Stevenson <dstevenson@uplandsoftware.com>
2019-06-02 23:45:21 +0100

    Can now create multi-tag filters, and a helper does the mangling of query params

--- lib/NeverTire/Helper/TagQueryParams.pm
+++ lib/NeverTire/Helper/TagQueryParams.pm
@@ -18,22 +18,44 @@ sub register {
         my $qp = $c->param('tags');
         return '' unless $qp;
 
-        my @current_ids = split(/,/, $qp);
-        my @new_ids = ();
+        my @ids = split(/,/, $qp);
 
-        my $remove_id = $tag->id;
-        foreach my $id ( @current_ids ) {
-            push @new_ids, $id unless $id == $remove_id;
-        }
+        @ids = _remove_element($tag->id, @ids);
 
-        return '' unless scalar(@new_ids);
+        return '' unless scalar(@ids);
 
-        return '?tags=' . join(',', @new_ids);
+        return '?tags=' . join(',', @ids);
+    });
+
+    # $tag is a Tag result object
+    # Returns ?tags=n,m,p'
+    $app->helper(add_tag_to_query_params => sub {
+        my ($c, $tag) = @_;
+
+        my $qp = $c->param('tags');
+        my @ids = $qp ? split(/,/, $qp) : ();
+
+        my $new_tag_id = $tag->id;
+        @ids = _remove_element($new_tag_id, @ids);
+        push @ids, $new_tag_id;
+
+        return '?tags=' . join(',', @ids);
     });
 
   
 
     # TODO Tests for helpers
+}
+
+sub _remove_element {
+    my ($element, @old_array) = @_;
+
+    my @new_array;
+    foreach my $id (@old_array) {
+        push @new_array, $id unless $id == $element;
+    }
+
+    return @new_array;
 }
 
 1;
