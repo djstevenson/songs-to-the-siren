56e381178b5e6efb4b86044462ce49be5e4f91e4
David Stevenson <david@ytfc.com>
2019-06-05 00:15:04 +0100

    Move authorization-related methods from user result class into a role

--- lib/NeverTire/Schema/Result/User.pm
+++ lib/NeverTire/Schema/Result/User.pm
@@ -6,11 +6,13 @@ extends 'NeverTire::Schema::Base::Result';
 
 # TODO POD
 
-use NeverTire::Util::Password qw/ random_user_key /;
-
 use DateTime;
 use Text::Markdown qw/ markdown /;
 use Carp qw/ croak /;
+
+# Bring in methods that we split out. We did that to keep
+# file sizes more managable, and to group related methods
+with 'NeverTire::Schema::Role::User::Auth';
 
 __PACKAGE__->load_components('InflateColumn::DateTime');
 
@@ -64,51 +66,6 @@ sub edit_song {
     $song->update($full_args);
     
     return $song;
-}
-
-sub generate_new_user_key {
-    my ($self, $purpose, $duration) = @_;
-
-    # TODO Sort out exports from NeverTire::Util::Password
-    my $key = random_user_key();
-
-    my $user_key_rs = $self->result_source->schema->resultset('UserKey');
-    $user_key_rs->create_key({
-        user_id  => $self->id,
-        key      => $key,
-        purpose  => $purpose,
-        duration => $duration,
-    });
-
-    # Yes, returns the plain text key, not the hash. This is what
-    # we need to embed in (say) registration emails
-    return $key;
-}
-
-sub send_registration_email {
-    my $self = shift;
-
-    my $purpose = 'registration';
-    my $duration = DateTime::Duration->new(hours => 1); # TODO Configurable
-    my $user_key = $self->generate_new_user_key($purpose, $duration);
-
-    $self->send_email($purpose, {
-        user => $self->id,
-        key  => $user_key,
-    });
-}
-
-sub send_email {
-    my ($self, $template_name, $data) = @_;
-
-    my $email_rs = $self->result_source->schema->resultset('Email');
-    $email_rs->create({
-        email_from    => 'noreply@ytfc.com', # TODO Make configurable
-        email_to      => lc($self->email),
-        template_name => $template_name,
-        data          => $data,
-        queued_at     => DateTime->now,
-    });
 }
 
 no Moose;
