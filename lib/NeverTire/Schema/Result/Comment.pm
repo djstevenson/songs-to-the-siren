78f0612a1581ca30c128201f993a2e147cf0a9c0
David Stevenson <david@ytfc.com>
2019-06-08 00:13:34 +0100

    New relationship: Comments can be replies to other comments

--- lib/NeverTire/Schema/Result/Comment.pm
+++ lib/NeverTire/Schema/Result/Comment.pm
@@ -17,6 +17,8 @@ __PACKAGE__->add_columns(
     song_id          => {data_type => 'INTEGER'},
     author_id        => {data_type => 'INTEGER'},
     
+    parent_id        => {data_type => 'INTEGER'},
+
     comment_markdown => {data_type => 'TEXT'},
     comment_html     => {data_type => 'TEXT'},
     full_markdown    => {data_type => 'TEXT'},
@@ -28,8 +30,12 @@ __PACKAGE__->add_columns(
 
 __PACKAGE__->set_primary_key('id');
 
-__PACKAGE__->belongs_to(author => 'NeverTire::Schema::Result::User',   { 'foreign.id' => 'self.author_id' });
-__PACKAGE__->belongs_to(song   => 'NeverTire::Schema::Result::Song',   { 'foreign.id' => 'self.song_id' });
+__PACKAGE__->belongs_to(author => 'NeverTire::Schema::Result::User',    { 'foreign.id' => 'self.author_id' });
+
+__PACKAGE__->belongs_to(parent => 'NeverTire::Schema::Result::Comment', { 'foreign.id' => 'self.parent_id' });
+__PACKAGE__->has_many(replies  => 'NeverTire::Schema::Result::Comment', { 'foreign.parent_id' => 'self.id' });
+
+__PACKAGE__->belongs_to(song   => 'NeverTire::Schema::Result::Song',    { 'foreign.id' => 'self.song_id' });
 
 no Moose;
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
