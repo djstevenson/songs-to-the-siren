00b50da01576e882471c9c33f8f153d4c9a5f239
David Stevenson <david@ytfc.com>
2019-06-20 22:44:54 +0100

    Implement user-registration form with new form buttons

--- lib/NeverTire/Form/User/Register.pm
+++ lib/NeverTire/Form/User/Register.pm
@@ -6,8 +6,8 @@ use NeverTire::Form::Moose;
 extends 'NeverTire::Form::Base';
 with 'NeverTire::Form::Role';
 
-has '+submit_label' => (default => 'Register user');
-has '+id'           => (default => 'register-user');
+has '+legend' => (default => 'Register user');
+has '+id'     => (default => 'register-user');
 
 has_field name => (
     type        => 'Input::Text',
@@ -46,6 +46,8 @@ has_field password => (
     	[ MaxLength => {max => 99} ],
 	],
 );
+
+has_button register_user => ();
 
 override posted => sub {
 	my $self = shift;
