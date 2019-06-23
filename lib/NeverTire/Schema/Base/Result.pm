5238c9aaca66439ebee94bdc7de022d307abf1fc
David Stevenson <david@ytfc.com>
2019-06-23 21:45:40 +0100

    Add use strict/warnings where it was missing

--- lib/NeverTire/Schema/Base/Result.pm
+++ lib/NeverTire/Schema/Base/Result.pm
@@ -1,6 +1,9 @@
 package NeverTire::Schema::Base::Result;
 use base 'DBIx::Class::Core';
 
+use strict;
+use warnings;
+
 # Asserts that we are using the test DB, throwing an
 # exception if not. Used in code that exists to support
 # tests but that shouldn't be available in production
