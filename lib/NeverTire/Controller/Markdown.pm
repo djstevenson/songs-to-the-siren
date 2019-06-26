c444331c86e05326fd7440395148fa77e77e613a
David Stevenson <david@ytfc.com>
2019-06-26 22:43:10 +0100

    Mojolicious doesn't like an action called 'render'

--- lib/NeverTire/Controller/Markdown.pm
+++ lib/NeverTire/Controller/Markdown.pm
@@ -6,14 +6,14 @@ use Text::Markdown qw/ markdown /;
 sub add_routes {
     my ($c, $r) = @_;
 
-    $r->route('/markdown')->via('POST')->to('markdown#render');
+    $r->route('/markdown')->via('POST')->to('markdown#render_markdown');
 }
 
-sub render {
+sub render_markdown {
     my $c = shift;
 
     $c->assert_user;
-    
+     
     my $markdown = $c->param('markdown');
     my $html = markdown($markdown);
     $c->render(text => $html);
