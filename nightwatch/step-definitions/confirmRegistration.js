0acb58123686ec7d38d32ed656ec4185dfecff57
David Stevenson <david@ytfc.com>
2019-07-17 21:51:13 +0100

    Bring in viewTestEmail page

--- nightwatch/step-definitions/confirmRegistration.js
+++ nightwatch/step-definitions/confirmRegistration.js
@@ -1,6 +1,8 @@
 const { client } = require('nightwatch-api');
 const { Given, Then, When } = require('cucumber');
 
+const viewTestEmail = client.page.viewTestEmail();
+
 When(/^I click on the confirm link in the registration email for (.*)$/, (username) => {
     return client
         .url(testEmail.url('registration', username))
