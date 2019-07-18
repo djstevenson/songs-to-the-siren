7f8ac8223976a8f1d68aa2669e4ddb829869d007
David Stevenson <david@ytfc.com>
2019-07-18 23:30:57 +0100

    More progress on confirm-reg tests

--- nightwatch/step-definitions/confirmRegistration.js
+++ nightwatch/step-definitions/confirmRegistration.js
@@ -4,7 +4,10 @@ const { Given, Then, When } = require('cucumber');
 const viewTestEmail = client.page.viewTestEmail();
 
 When(/^I click on the confirm link in the registration email for (.*)$/, (username) => {
-    return client
+    client
         .url(viewTestEmail.url('registration', username))
+        .waitForElementVisible('body', 1000);
+    viewTestEmail.section.urls.clickLink('@goodConfirm');
+    return client.waitForElementVisible('body', 1000);
 });
 
