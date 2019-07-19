c3df7da1dbc16507a5f85addcfb4043d44a90dd1
David Stevenson <david@ytfc.com>
2019-07-19 23:34:02 +0100

    Fix clicking on 'good confirm' link

--- nightwatch/pages/viewTestEmail.js
+++ nightwatch/pages/viewTestEmail.js
@@ -1,21 +1,5 @@
-const linkCommands = {
-	clickLink(name) {
-		return this
-			.click(name);
-	},
-};
 module.exports = {
 	url: function(type, name) {
 		return `${this.api.launchUrl}/test/view_email/${type}/${name}`;
-	},
-	sections: {
-		urls: {
-			selector: "#email",
-			elements: {
-				goodConfirm: "#email-urls-good-confirm > a",
-				goodDecline: "#email-urls-good-decline > a",
-			},
-			commands: [linkCommands]
-		}
 	}
 };
--- nightwatch/step-definitions/confirmRegistration.js
+++ nightwatch/step-definitions/confirmRegistration.js
@@ -4,10 +4,10 @@ const { Given, Then, When } = require('cucumber');
 const viewTestEmail = client.page.viewTestEmail();
 
 When(/^I click on the confirm link in the registration email for (.*)$/, (username) => {
-    client
+    return client
         .url(viewTestEmail.url('registration', username))
+        .waitForElementVisible('body', 1000)
+        .click('#email-urls-good-confirm > a')
         .waitForElementVisible('body', 1000);
-    viewTestEmail.section.urls.clickLink('@goodConfirm');
-    return client.waitForElementVisible('body', 1000);
 });
 
