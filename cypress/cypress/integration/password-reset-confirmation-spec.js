819793a0992143f884b51a347d2b827b9e5bb1b6
David Stevenson <david@ytfc.com>
2019-07-28 22:02:12 +0100

    Add some tests for bad password reset requests

--- cypress/cypress/integration/password-reset-confirmation-spec.js
+++ cypress/cypress/integration/password-reset-confirmation-spec.js
@@ -8,7 +8,7 @@ import { LoginPage } from '../pages/login-page';
 
 var newUser = new UserFactory('pwresetconf')
 
-describe('Password reset confirmation (and declination)', function() {
+describe('Password reset confirmation', function() {
     describe('User can perform successful reset', function() {
         it('reset-confirm shows right shiz', function() {
 
@@ -37,6 +37,47 @@ describe('Password reset confirmation (and declination)', function() {
                 .login(user.getName(), newPassword)
                 .assertLoggedInAs(user.getName())
 
+        })
+    })
+    
+    describe('Invalid reset requests are rejected', function() {
+        it('invalid reset code gives page-not-found', function() {
+
+            const user = newUser
+                .getNextRegisteredUser()
+                .confirmRegistration()
+            
+            new ForgotPasswordPage()
+                .visit()
+                .forgotPassword(user.getEmail())
+
+            new TestEmailPage()
+                .visit('password_reset', user.getName())
+                .badConfirmReset()
+            
+            cy.assertPageNotFound()
+
+        })
+    })
+    
+    describe('Invalid new passwords are rejected', function() {
+        it('bad new passwords give right errors', function() {
+
+            const user = newUser
+                .getNextRegisteredUser()
+                .confirmRegistration()
+            
+            new ForgotPasswordPage()
+                .visit()
+                .forgotPassword(user.getEmail())
+
+            new TestEmailPage()
+                .visit('password_reset', user.getName())
+                .confirmReset()
+            
+            new ResetPasswordPage()
+                .resetPassword('').assertFormError('password', 'Required')
+                .resetPassword('abc').assertFormError('password', 'Minimum length 5')
         })
     })
     
