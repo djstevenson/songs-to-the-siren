31a4b6b9b954efa6663dec3bca29c14fbd15ed60
David Stevenson <david@ytfc.com>
2019-07-31 23:31:24 +0100

    Add some page access tests for admin users

--- cypress/integration/page-access-checks-spec.js
+++ cypress/integration/page-access-checks-spec.js
@@ -54,8 +54,41 @@ describe('Access control depending on user authorisation', function() {
                 .assertVisitError(403)
         })
         it('can not access song-create admin page', function() {
+            const user = newUser.getNextConfirmedUser().login()
             new SongCreatePage()
                 .assertVisitError(403)
+        })
+    })
+
+    describe('Access for logged-in admin user', function() {
+        it('can access login page', function() {
+            newUser.getNextConfirmedUser(true).login()
+            new LoginPage()
+                .visit()
+                .assertLoggedOut()    // Login page logs you out
+                .assertTitle('Login')
+
+        })
+        it('can access home page', function() {
+            const user = newUser.getNextConfirmedUser(true).login()
+            new HomePage()
+                .visit()
+                .assertLoggedInAs(user.getName())
+                .assertTitle('Songs I Will Never Tire of')
+        })
+        it('can access song-list admin page', function() {
+            const user = newUser.getNextConfirmedUser(true).login()
+            new SongListPage()
+                .visit()
+                .assertLoggedInAs(user.getName())
+                .assertTitle('Song list')
+        })
+        it('can not access song-create admin page', function() {
+            const user = newUser.getNextConfirmedUser(true).login()
+            new SongCreatePage()
+                .visit()
+                .assertLoggedInAs(user.getName())
+                .assertTitle('New song')
         })
     })
 })
