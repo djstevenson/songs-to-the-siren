6d56353eb7ce62aca501146d25b78b7ac7ac5503
David Stevenson <david@ytfc.com>
2019-08-04 01:10:14 +0100

    create-song form validation tests

--- cypress/integration/create-song-spec.js
+++ cypress/integration/create-song-spec.js
@@ -42,8 +42,9 @@ describe('Create Song tests', function() {
 
         it('Form shows right errors with invalid input', function() {
 
-            // Not practical to check every validation option, but
-            // do the basics
+            // There's deliberately minimal validation, no reason
+            // why I shouldn't be able to enter a single-character
+            // title for example.
             userFactory
                 .getNextConfirmedUser(true)
                 .login()
@@ -59,13 +60,32 @@ describe('Create Song tests', function() {
                     summaryMarkdown: 'a',
                     fullMarkdown:    'a'
                 })
-                .assertFormError('title',           'Required')
-                .assertFormError('artist',          'Required')
-                .assertFormError('album',           'Required')
-                .assertFormError('countryId',       'Required')
-                .assertFormError('releasedAt',      'Required')
-                .assertFormError('summaryMarkdown', 'Required')
-                .assertFormError('fullMarkdown',    'Required')
+                .assertNoFormError('title')
+                .assertNoFormError('artist')
+                .assertNoFormError('album')
+                .assertFormError('countryId', 'Invalid number')
+                .assertNoFormError('releasedAt')
+                .assertNoFormError('summaryMarkdown')
+                .assertNoFormError('fullMarkdown')
+
+            new CreateSongPage()
+                .visit()
+                .createSong({
+                    title:           'a',
+                    artist:          'a',
+                    album:           'a',
+                    countryId:       '0',
+                    releasedAt:      'a',
+                    summaryMarkdown: 'a',
+                    fullMarkdown:    'a'
+                })
+                .assertNoFormError('title')
+                .assertNoFormError('artist')
+                .assertNoFormError('album')
+                .assertFormError('countryId', 'Country id 0 does not exist')
+                .assertNoFormError('releasedAt')
+                .assertNoFormError('summaryMarkdown')
+                .assertNoFormError('fullMarkdown')
         })
     })
 
