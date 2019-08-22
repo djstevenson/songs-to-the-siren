332537b74821884a969c53e574488ea57d99e23c
David Stevenson <david@ytfc.com>
2019-08-23 00:45:19 +0100

    Tests for admin user link

---
+++ cypress/integration/admin-links-spec.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+import { HomePage     } from '../pages/home-page'
+import { UserFactory  } from '../support/user-factory'
+
+const label = 'adminlinks';
+const userFactory = new UserFactory(label);
+
+context('Admin user links to song-list', () => {
+    describe('Admin user has song-list link', () => {
+        it('link exists', () => {
+            cy.resetDatabase()
+
+            const user = userFactory.getNextLoggedInUser(true)
+
+            // Should go to home page which has admin link
+            // Click on it to go to song list which also has same link
+            new HomePage()
+                .visit()
+                .assertLoggedInAsAdmin('admin')
+                .clickSongListLink()
+                .assertLoggedInAsAdmin('admin')
+        })
+    })
+
+})
