e44ba3551bd1123d5cefc4683b57bb608e4725f5
David Stevenson <david@ytfc.com>
2019-08-30 21:58:20 +0100

    Start on pages to serve markdown static content

--- tools/test_data.sql
+++ tools/test_data.sql
@@ -1,3 +1,5 @@
+INSERT INTO pages ("name", title, markdown, html, updated_at) VALUES ('about', 'About page title', 'The about page', '<p>The about page</p>', NOW());
+
 INSERT INTO countries ("name", emoji) VALUES ('UK', '&#x1F1EC;&#x1F1E7;');
 INSERT INTO countries ("name", emoji) VALUES ('US', '&#x1F1FA;&#x1F1F8;');
 INSERT INTO countries ("name", emoji) VALUES ('CA', '&#x1F1E8;&#x1F1E6;');
