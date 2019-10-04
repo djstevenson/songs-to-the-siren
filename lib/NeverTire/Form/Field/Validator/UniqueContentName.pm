07d9a877760540798eff5dd60955aeb8fd41c706
David Stevenson <david@ytfc.com>
2019-10-05 00:08:40 +0100

    ISS-158 Refer to 'content'

--- lib/NeverTire/Form/Field/Validator/UniqueContentName.pm
+++ lib/NeverTire/Form/Field/Validator/UniqueContentName.pm
@@ -7,10 +7,10 @@ with 'NeverTire::Form::Field::Validator::Role';
 sub validate{
 	my ($self, $value) = @_;
 
-	my $pages_rs = $self->schema->resultset('Content');
-	my $found = $pages_rs->find($value);
+	my $content_rs = $self->schema->resultset('Content');
+	my $found = $content_rs->find($value);
 	
-    return 'Name already in use' if $found;
+    return 'Content name already in use' if $found;
 	return undef;
 }
 
