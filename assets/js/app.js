053a1444516c8187738473cdb2919a5f5bfce2a8
David Stevenson <david@ytfc.com>
2019-12-01 22:48:26 +0000

    Better JS implementation of deleting tags

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,14 @@
-function deleteSongTag(songID, tagID) {
-    var url = "/admin/song/" + songID + "/tag/" + tagID;
-    $.ajax({
-        type: "DELETE",
-        url: url,
-        success: function(data) {
-            var buttonID = "#song-tag-" + songID + "-" + tagID;
-            var listItemID = buttonID + "-li";
-            $(buttonID).hide(150, function(){
-                this.remove();
-                $(listItemID).remove();
-            });
-        },
-    });
-}
-
 $(function() {
-    $('.song-tag').hover(
-        function() { $(this).toggleClass('btn-primary');  $(this).toggleClass('btn-outline-dark') }
-    );
-    $('.song-tag-remove').hover(
-        function() { $(this).toggleClass('btn-danger');  $(this).toggleClass('btn-outline-dark') }
-    );
+    $('.song-tag').hover( function() {
+        $(this).toggleClass('btn-primary')
+        $(this).toggleClass('btn-outline-dark')
+    })
+    $('.song-tag-remove').hover( function() {
+        $(this).toggleClass('btn-danger')
+        $(this).toggleClass('btn-outline-dark')
+    })
 
+    
     $('[data-markdown-preview]').on('change keyup paste focus', $.throttle( 500, function() {
         const data = $(this).data()
         const previewAreaId = '#' + data['markdownPreview']
@@ -30,9 +17,30 @@ $(function() {
             url: "/markdown",
             data: { markdown: $(this).val() },
             success: function(data) {
-                $(previewAreaId).html(data).show();
+                $(previewAreaId).html(data).show()
             },
-        });
-    }));
-});
+        })
+    }))
+
+    $('[data-delete-song-tag]').on('click', function() {
+        const data = $(this).data()
+        const buttonID = $(this).attr('id')
+        const tagId = data['tagId']
+        const songId = data['songId']
+        var url = "/admin/song/" + songId + "/tag/" + tagId
+        const button = $(this)
+        $.ajax({
+            type: "DELETE",
+            url: url,
+            success: function(data) {
+                var listItemID = buttonID + "-li"
+                button.hide(150, function() {
+                    $(this).remove()
+                    $(listItemID).remove()
+                })
+            },
+        })
+    })
+
+})
 
