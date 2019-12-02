fbc6da97e2668c30844e7e29883ff2194bb33e23
David Stevenson <david@ytfc.com>
2019-12-02 00:41:34 +0000

    Fix tag deletion interface

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,14 +29,15 @@ $(function() {
         const songId = data['songId']
         var url = "/admin/song/" + songId + "/tag/" + tagId
         const button = $(this)
+        const listItemID = '#' + buttonID + "-li"
+        console.log("list item "+ listItemID)
+        const listItem = $(listItemID)
         $.ajax({
             type: "DELETE",
             url: url,
             success: function(data) {
-                var listItemID = buttonID + "-li"
                 button.hide(150, function() {
-                    $(this).remove()
-                    $(listItemID).remove()
+                    listItem.remove()
                 })
             },
         })
