3dee3faa69c91573d7c0c621915e227b62df1e73
David Stevenson <david@ytfc.com>
2019-12-11 21:42:21 +0000

    Permissions changes to pgsql scripts for easier non-Mac deployment

--- tools/initdb.test.sh
+++ tools/initdb.test.sh
@@ -2,7 +2,7 @@
 
 cd tools
 echo 'Drop :test database'
-psql -U nevertire postgres -c 'drop database never_tire_test'
+psql -U nevertire postgres -c 'DROP DATABASE IF EXISTS never_tire_test'
 
 echo 'Create :test database'
-createdb -O nevertire never_tire_test
+createdb -U postgres -O nevertire never_tire_test
